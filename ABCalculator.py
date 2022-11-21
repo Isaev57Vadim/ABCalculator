54ab682946ed1ac7a28c8e07e8051c4570003ba5
Isaev57Vadim <115579097+Isaev57Vadim@users.noreply.github.com>
2022-11-21 22:09:33 +0300

    Добавление заголовка и кнопок

--- ABCalculator.py
+++ ABCalculator.py
@@ -2,10 +2,26 @@
 
 import tkinter as tk
 
+# Функция закрытия программы
+def do_close():
+    root.destroy()
+
 # Сооздание главного окна
 root = tk.Tk()
 root.geometry("280x300")
 root.title("A/B калькулятор")
+
+# Добавление метки заголовка
+lblTitle = tk.Label(text = "A/B калькулятор", font = ('ALGERIAN', 15, 'bold'), fg = '#ff0011')
+lblTitle.place(x=45, y=20)
+
+# Добавление кнопки "Рассчитать"
+btnProcess = tk.Button(root, text="Рассчитать", font = ('Helvetica', 10, 'bold'))
+btnProcess.place(x=25, y=250, width=90, height=30)
+
+# Добавление кн закрытия программы
+btnClose = tk.Button(root, text="Закрыть", font = ('Helvetica', 10, 'bold'), command=do_close)
+btnClose.place(x=160, y=250, width=90, height=30)
 
 # Запуск цикла mainloop
 root.mainloop()
